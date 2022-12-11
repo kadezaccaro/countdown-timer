fc829bf961ce512082dec837bc1aa8c326f391b6
kade <zaccaro.kade@gmail.com>
2022-12-11 12:27:47 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const endingDate = document.querySelector(".ending-date");
+const deadlineContainer = document.querySelector(".deadline-container");
+const items = document.querySelectorAll(".deadline-format h1");
+
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 7, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+const month = months[futureDate.getMonth()];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+endingDate.textContent = `${weekday}, ${month} ${date}, ${year} at ${hours}:${minutes}am`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+
+const getRemainingTime = () => {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60min
+  // 1d = 24hr
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  // calculate all values
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  // set values array
+  const values = [days, hours, minutes, seconds];
+
+  const zeroFormat = (item) => {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  };
+
+  items.forEach((item, index) => {
+    item.innerHTML = zeroFormat(values[index]);
+  });
+
+  if (t < 0) {
+    clearInterval(countdown);
+    deadlineContainer.innerHTML = `<h1 class="expired">The countdown has ended.</h1>`;
+  }
+};
+
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
